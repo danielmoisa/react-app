008fb1dd6f14784f42bd83f4b79b8897e62723be
danielmoisa <danimoisa93@gmail.com>
2019-12-30 18:43:28 +0200

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import SignIn from './components/auth/SignIn';
+import Home from './components/pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: { main: '#0c9463' },
+      secondary: { main: '#4d80e4' },
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <BrowserRouter>
+        <div className="App">
+        </div>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/sign-in">
+            <SignIn />
+          </Route>
+        </Switch>
+    </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
